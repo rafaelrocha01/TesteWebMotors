b33dfd5614bb7e2bc0521fcb6ee303b18e63b70f
rafaelrocha01 <rafaeldafv@gmail.com>
2021-02-12 22:47:05 -0300

    Update Parametro.java
    Excluindo variáveis não utilizadas

--- src/test/java/org/br/webmotors/config/Parametro.java
+++ src/test/java/org/br/webmotors/config/Parametro.java
@@ -4,6 +4,4 @@ public class Parametro {
 
     public final static String URL = "https://www.webmotors.com.br/";
     public final static int IMPLICITWAIT = 30;
-    public final static String USER = "Rafael Jesus";
-    public final static String SENHA = "8907";
 }
